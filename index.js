dd521befc7f06f00b5319d496128e38a04996c4e
NibeshSdev <sdev.nibs@gmail.com>
2017-09-10 23:54:50 +0545

    added JWT auth

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+'use strict';
+
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000,
 mongoose= require('mongoose'),
-Task= require('./api/models/todoListModel'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+jsonwebtoken = require('jsonwebtoken'),
+User = require('./api/models/userModel'),
+Task= require('./api/models/todoListModel');
+
 
 
 mongoose.Promise = global.Promise;
@@ -15,6 +20,18 @@ mongoose.connect('mongodb://localhost/tododb', {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next){
+  if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT'){
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode){
+      if(err) res.user = undefined;
+      req.user = decode;
+      next();
+    });
+  }else{
+    res.user = undefined;
+    next();
+  }
+});
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
@@ -26,3 +43,5 @@ app.use(function(req, res){
 app.listen(port);
 
 console.log('App is listening to http://localhost:'+port);
+
+module.exports = app;
